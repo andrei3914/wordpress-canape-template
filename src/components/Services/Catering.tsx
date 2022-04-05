4e11349c236973462b02c7b1c5e26cfaf603e0e6
andrei3914 <popandrei230@yahoo.com>
2022-04-05 14:27:05 +0300

    private chef services section added

--- src/components/Services/Catering.tsx
+++ src/components/Services/Catering.tsx
@@ -8,7 +8,7 @@ const Catering = () => {
     <div>
       <Navigation />
       <div className="catering-container">
-        <div className="hero">
+        <div className="hero-catering">
           <img src="https://canapedemo.files.wordpress.com/2015/12/eating-601578_1920.jpg?w=1180&h=530&crop=1" alt="dish"
             srcSet='https://canapedemo.files.wordpress.com/2015/12/eating-601578_1920.jpg?w=1180&h=530&crop=1 1180w, https://canapedemo.files.wordpress.com/2015/12/eating-601578_1920.jpg?w=150&h=67&crop=1 150w, https://canapedemo.files.wordpress.com/2015/12/eating-601578_1920.jpg?w=300&h=135&crop=1 300w, https://canapedemo.files.wordpress.com/2015/12/eating-601578_1920.jpg?w=768&h=345&crop=1 768w, https://canapedemo.files.wordpress.com/2015/12/eating-601578_1920.jpg?w=1024&h=460&crop=1 1024w' />
         </div>
--- src/components/Services/PrivateChef.tsx
+++ src/components/Services/PrivateChef.tsx
@@ -6,7 +6,16 @@ const PrivateChef = () => {
   return (
     <div>
         <Navigation />
-        PrivateChef
+        <div className="privateChef-container">
+          <div className="hero-private">
+            <img src="https://canapedemo.files.wordpress.com/2015/12/cutlery-865096_1280.jpg?w=1180&h=530&crop=1" alt=""
+            srcSet='https://canapedemo.files.wordpress.com/2015/12/cutlery-865096_1280.jpg?w=1180&h=530&crop=1 1180w, https://canapedemo.files.wordpress.com/2015/12/cutlery-865096_1280.jpg?w=150&h=67&crop=1 150w, https://canapedemo.files.wordpress.com/2015/12/cutlery-865096_1280.jpg?w=300&h=135&crop=1 300w, https://canapedemo.files.wordpress.com/2015/12/cutlery-865096_1280.jpg?w=768&h=345&crop=1 768w, https://canapedemo.files.wordpress.com/2015/12/cutlery-865096_1280.jpg?w=1024&h=460&crop=1 1024w' />
+          </div>
+
+          <div className="private-content">
+            <h1 className='entry-title'>Private Chef Services</h1>
+          </div>
+        </div>
         <Footer />
     </div>
   )
